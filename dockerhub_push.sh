ed58d59f31190c557e7ba1a04ef0aa4ead11bf4f
raviasthana <ravi.asthana@gmail.com>
2019-12-17 21:23:32 +0530

    kubernetes config files for deployment

--- dockerhub_push.sh
+++
@@ -1,5 +0,0 @@
-docker login -u $DOCKER_ID -p $DOCKER_PWD
-docker push -t rasthanadocker/multi-docker-client
-docker push -t rasthanadocker/multi-docker-nginx
-docker push -t rasthanadocker/multi-docker-server
-docker push -t rasthanadocker/multi-docker-worker
